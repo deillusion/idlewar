6a2f1da0bdd97301c921bf8c075e7df3fcd846a2
deillusion <15611376180@163.com>
2024-11-06 15:08:15 +0800

    init

---
+++ src/settings.ce740.js
@@ -0,0 +1,25 @@
+window._CCSettings = {
+    platform: "android",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/Loading.fire",
+    orientation: "",
+    server: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "4210e",
+        resources: "40b28",
+        main: "e2452"
+    }
+};
